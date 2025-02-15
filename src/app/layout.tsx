bf1ae6e3bf72ed52404b42806afe4c87931af205
taylorallen <twhitallen07@gmail.com>
2025-02-15 12:02:31 -0600

    updated file structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import './styles/globals.css'
+import "./styles/globals.css";
+import { Navigation } from "@/components/navigation";
 
 export const metadata: Metadata = {
   title: "React Playground",
@@ -11,8 +12,14 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className="antialiased">
+      <body className="bg-gray-100">
+        <header className="bg-slate-900 text-white p-4 text-center">
+          <Navigation />
+        </header>
         {children}
+        <footer className="bg-slate-900 text-white p-4 text-center">
+          Made with ❤️ by {"React Playground"}
+        </footer>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,10 @@
-import React from "react";
-// import Navbar from "@/components/ui/navbar";
-import { tutorialsArray } from "@/tutorials";
+"use client";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import TransitionCards from "@/components/transitionCards";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
 
 export default function Home() {
   return (
     <div className="container justify-items-center m-8">
-       <div className="navbar m-8">
-        <NavigationMenu>
-          <NavigationMenuList>
-            {/* This is where the Tutorials tab link starts */}
-            <NavigationMenuItem>
-              <NavigationMenuTrigger className="w-48">
-                Tutorials
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink>
-                  {tutorialsArray && tutorialsArray.length > 0
-        ? tutorialsArray.map((tutorial, tutorialIndex) => {
-            return  <TransitionCards tutorial={tutorial} />}
-                </NavigationMenuLink>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu> 
-      </div> 
-      {/* <Navbar/> */}
-
-      <h1>Here are my tutorials!:</h1>
-
       <Button className="mt-8">
         <Link href={"https://ui.shadcn.com/docs/components/button"}>
           Click My ShadCN Button
