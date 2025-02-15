de87a5b735c438aee77563e91dfaa9bc4d15bf03
taylorallen <twhitallen07@gmail.com>
2025-02-15 11:28:03 -0600

    playing with navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,52 @@
 import React from "react";
+// import Navbar from "@/components/ui/navbar";
 import { tutorialsArray } from "@/tutorials";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
 
 export default function Home() {
   return (
     <div className="container justify-items-center m-8">
+       <div className="navbar m-8">
+        <NavigationMenu>
+          <NavigationMenuList>
+            {/* This is where the Tutorials tab link starts */}
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className="w-48">
+                Tutorials
+              </NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>
+                  {tutorialsArray && tutorialsArray.length > 0
+                    ? tutorialsArray.map((tutorial, tutorialIndex) => {
+                        return (
+                          <div
+                            className="w-96"
+                            key={tutorialIndex + "tutorials"}
+                          >
+                            {tutorial.name}
+                          </div>
+                        );
+                      })
+                    : "no tutorials"}
+                </NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu> 
+      </div> 
+      {/* <Navbar/> */}
+
       <h1>Here are my tutorials!:</h1>
-      {tutorialsArray && tutorialsArray.length > 0
-        ? tutorialsArray.map((tutorial, tutorialIndex) => {
-            return <div key={tutorialIndex + "tutorials"}>{tutorial.name}</div>;
-          })
-        : "no tutorials"}
+
       <Button className="mt-8">
         <Link href={"https://ui.shadcn.com/docs/components/button"}>
           Click My ShadCN Button
