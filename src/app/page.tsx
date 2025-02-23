2fada8e68d31faff13eb1c0f8c62938c1df08ef6
Alex <alexander.ayala.palacin@gmail.com>
2025-02-23 18:12:52 -0500

    tutorial single view on userflow functionality working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,22 @@ import { tutorialsArray } from "@/app/assets/tutorials";
 import { Tutorial } from '@/app/assets/types';
 import 'animate.css';
 import CategoriesView from '@/components/categoriesView';
+import { categoriesArray } from './assets/categories';
+import Link from "next/link";
 
-const categoryArray = ["All Categories","Authentication","Security", "UI/UX"];
 
 
 const icon1= "M488-278h66.67v-404H406v66.67h82V-278ZM186.67-120q-27 0-46.84-19.83Q120-159.67 120-186.67v-586.66q0-27 19.83-46.84Q159.67-840 186.67-840h586.66q27 0 46.84 19.83Q840-800.33 840-773.33v586.66q0 27-19.83 46.84Q800.33-120 773.33-120H186.67Zm0-66.67h586.66v-586.66H186.67v586.66Zm0-586.66v586.66-586.66Z";
 const icon2= "M363.33-278h233.34v-66.67H430V-448h100q27 0 46.83-19.83 19.84-19.84 19.84-46.84v-100.66q0-27-19.84-46.84Q557-682 530-682H363.33v66.67H530v100.66H430q-27 0-46.83 19.84Q363.33-475 363.33-448v170ZM186.67-120q-27 0-46.84-19.83Q120-159.67 120-186.67v-586.66q0-27 19.83-46.84Q159.67-840 186.67-840h586.66q27 0 46.84 19.83Q840-800.33 840-773.33v586.66q0 27-19.83 46.84Q800.33-120 773.33-120H186.67Zm0-66.67h586.66v-586.66H186.67v586.66Zm0-586.66v586.66-586.66Z";
 const icon3="M363.33-278H530q27 0 46.83-19.83 19.84-19.84 19.84-46.84v-78q0-26.66-15.34-42.66-15.33-16-37.33-16 22 0 37.33-14.67 15.34-14.67 15.34-42v-77.33q0-27-19.84-46.84Q557-682 530-682H363.33v66.67H530v100.66h-84.67V-448H530v103.33H363.33V-278ZM186.67-120q-27 0-46.84-19.83Q120-159.67 120-186.67v-586.66q0-27 19.83-46.84Q159.67-840 186.67-840h586.66q27 0 46.84 19.83Q840-800.33 840-773.33v586.66q0 27-19.83 46.84Q800.33-120 773.33-120H186.67Zm0-66.67h586.66v-586.66H186.67v586.66Zm0-586.66v586.66-586.66Z";
 
-const generateTutorialNames = (selectedCategory:string)=>{
+const generateFilteredTutorials = (selectedCategory:string)=>{
 
   const filteredTutorials = tutorialsArray.filter((tutorial: Tutorial)=> tutorial.category == selectedCategory);
   const availableTutorials = selectedCategory == "All Categories" ? tutorialsArray : filteredTutorials; 
   const tutorialNames = availableTutorials.map(
     (tutorial)=>{
-      return tutorial.name
+      return tutorial
   });
   return tutorialNames;
 }
@@ -28,6 +29,7 @@ const generateTutorialNames = (selectedCategory:string)=>{
 
 export default function Home() {
   const [category, setCategory]=useState("All Categories");
+  const [chosenTutorial, setChosenTutorial]=useState<Tutorial >(tutorialsArray[0]);
   
   return (
     <div className="container justify-items-center  ">
@@ -57,7 +59,7 @@ export default function Home() {
               </svg>
             <Dropdown 
               dropdownTitle="Choose a category" 
-              dropdownArray={categoryArray} 
+              dropdownCategoryArray={categoriesArray} 
               setCategory={setCategory}
             />
 
@@ -75,7 +77,8 @@ export default function Home() {
         
             <Dropdown 
               dropdownTitle="Choose a tutorial" 
-              dropdownArray={generateTutorialNames(category)} 
+              dropdownTutorialArray={generateFilteredTutorials(category)} 
+              setChosenTutorial={setChosenTutorial}
             />
             </div>
           <div className="navigate-to-tutoril">
@@ -89,11 +92,13 @@ export default function Home() {
             d={icon3}
           />
           </svg>
-            <button
-          
-            className={` tutorial-btn  flex shadow-react-glow items-center gap-2 px-3 py-2 rounded-md text-white bg-reactDarkBlue hover:bg-reactDarkBlue transition-colors `}>
-              Go 2 Tutorial
-            </button>
+          <Link href={`/singleTutorial/${chosenTutorial.id}`}>
+              <button
+            
+              className={` tutorial-btn  flex shadow-react-glow items-center gap-2 px-3 py-2 rounded-md text-white bg-reactDarkBlue hover:bg-reactDarkBlue transition-colors `}>
+                View Tutorial
+              </button>
+          </Link>
           </div>
         </div>
         <CategoriesView />
