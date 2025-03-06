87357a028a8bdf35c7da30379c143b7ceede0a9b
taylorallen <twhitallen07@gmail.com>
2025-03-06 14:30:18 -0600

    reviewing styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import 'animate.css';
 import CategoriesView from '@/components/categoriesView';
 import { categoriesArray } from './assets/categories';
 import Link from "next/link";
-import CategoryCard from '@/components/categoryCard';
+// import CategoryCard from '@/components/categoryCard';
 
 
 
@@ -41,9 +41,9 @@ export default function Home() {
             <div className="welcome-text-container">
               <h1 className="font-bold text-2xl">Level Up Your React Skills—</h1>
               <h1 className="font-bold text-2xl">One Tutorial at a Time</h1>
-              <p> We're two self-taught developers on a mission to break down the trickier parts of React—secure
+              <p> We are two self-taught developers on a mission to break down the trickier parts of React—secure
                  logins, chatbots, file uploads, and more. As we learn, we share step-by-step tutorials that are clear, practical, and beginner-friendly.
-                  Whether you're leveling up or just curious, join us as we tackle the stuff that used to give us anxiety—so it doesn’t have to give you any. 🚀
+                  Whether you are leveling up or just curious, join us as we tackle the stuff that used to give us anxiety—so it doesn’t have to give you any. 🚀
               </p>
             </div>
         </div>
