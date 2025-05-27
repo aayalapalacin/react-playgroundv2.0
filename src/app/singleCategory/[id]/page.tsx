30e1745beda3df3506a1c9486403aa78e62f6883
Alex <alexander.ayala.palacin@gmail.com>
2025-05-27 16:19:48 -0400

    added not found

--- src/app/singleCategory/[id]/page.tsx
+++ src/app/singleCategory/[id]/page.tsx
@@ -1,13 +1,7 @@
+// src/app/singleCategory/[id]/page.tsx
 import { categoriesArray } from "@/app/assets/categories";
-import { Categories } from "@/app/assets/types";
-// import TransitionCards from "@/components/transitionCards";
 import CategoryCard from "@/components/categoryCard";
-
-
-// interface TransitionCardsProps {
-//   tutorial: Tutorial;
-// }
-// export default function TransitionCards({ tutorial }: TransitionCardsProps) {
+import { notFound } from "next/navigation";
 
 interface PageProps {
   params: {
@@ -15,23 +9,25 @@ interface PageProps {
   };
 }
 
-export  function SingleCategory({ params }: PageProps ) {
-  
-  const idToInt:number = parseInt(params.id); 
-  
-  const selectedCategory: Categories = categoriesArray.filter((filterCategories)=> filterCategories.id == idToInt)[0];
+export default function Page({ params }: PageProps) {
+  const idToInt: number = parseInt(params.id);
+
+  const selectedCategory = categoriesArray.find(
+    (category) => category.id === idToInt
+  );
+
+  if (!selectedCategory) {
+    notFound(); // Optional: show 404 if ID is invalid
+  }
 
   return (
     <div className="container justify-items-center m-8 mx-auto w-3/4">
       <div className="singleCategoryTitle">
-        <h1
-        className=" m-7 text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight text-gray-900 dark:text-white"
-        >
-          {selectedCategory.name} 
-        Tutorials:
+        <h1 className="m-7 text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {selectedCategory?.name} Tutorials:
         </h1>
       </div>
-      <CategoryCard  selectedCategoryProp={selectedCategory.name}/>
+      <CategoryCard selectedCategoryProp={selectedCategory?.name || ""} />
     </div>
   );
 }
