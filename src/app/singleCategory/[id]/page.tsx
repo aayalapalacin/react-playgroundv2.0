45fac5868abb708f2c698fc8d619de702367778b
Alex <alexander.ayala.palacin@gmail.com>
2025-06-02 23:14:39 -0400

    more simple

--- src/app/singleCategory/[id]/page.tsx
+++ src/app/singleCategory/[id]/page.tsx
@@ -1,13 +1,9 @@
 // src/app/singleCategory/[id]/page.tsx
-import { notFound } from "next/navigation";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const idToInt = parseInt(params.id, 10);
-  if (isNaN(idToInt)) notFound();
-
+export default function Page({ params }: any) {
   return (
     <div>
-      <h1>Category ID: {idToInt}</h1>
+      <h1>ID: {params?.id}</h1>
     </div>
   );
 }
