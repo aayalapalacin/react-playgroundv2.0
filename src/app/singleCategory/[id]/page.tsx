bde419d13fc00dbd2643d3d1ff45638d6689fd05
Alex <alexander.ayala.palacin@gmail.com>
2025-06-02 23:38:32 -0400

    back

---
+++ src/app/singleCategory/[id]/page.tsx
@@ -0,0 +1,31 @@
+import { categoriesArray } from "@/app/assets/categories";
+import CategoryCard from "@/components/categoryCard";
+import { notFound } from "next/navigation";
+
+interface PageProps {
+  params: { id: string };
+}
+
+export default async function Page({ params }: PageProps) {
+  const idToInt = parseInt(params.id, 10);
+
+  const selectedCategory = categoriesArray.find(
+    (category) => category.id === idToInt
+  );
+
+  if (!selectedCategory) {
+    notFound();
+  }
+
+
+  return (
+    <div className="container justify-items-center m-8 mx-auto w-3/4">
+      <div className="singleCategoryTitle">
+        <h1 className="m-7 text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {selectedCategory.name} Tutorials:
+        </h1>
+      </div>
+      <CategoryCard selectedCategoryProp={selectedCategory.name} />
+    </div>
+  );
+}
--- src/app/singleCategory/[id]/page2.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/singleCategory/[id]/page.tsx
-
-export default function Page({ params }: any) {
-  return (
-    <div>
-      <h1>ID: {params?.id}</h1>
-    </div>
-  );
-}
